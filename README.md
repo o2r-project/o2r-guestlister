7ae96b522740aeb5ecfd0e52f897a6b14631f144
LukasLohoff <lukas.lohoff@googlemail.com>
2018-02-14 14:50:05 +0100

    add oauth dummy server based on the example implementation https://github.com/gerges-beshay/oauth2orize-examples

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # o2r-guestlister
-An OAuth server implementation to allow offline login with o2r-bouncer 
+
+An OAuth2 server implementation to allow offline login with o2r-bouncer 
+
+
+Based on the repository https://github.com/gerges-beshay/oauth2orize-examples and the underlying OAuth2 server implementation [oauth2orize](https://www.npmjs.com/package/oauth2orize).
+
+
+## Endpoints
+
+/login
+/logout
+/dialog/authorize
+...
+
+## Environment variables
+
---
+++ db/clients.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const clients = [
+  { id: '3', name: 'o2rtest', clientId: 'APP-8XINMK52KZVU', clientSecret: '2afa48e4-9473-446f-88bd', isTrusted: false }
+];
+
+
+module.exports.findById = (id, done) => {
+  for (let i = 0, len = clients.length; i < len; i++) {
+    if (clients[i].id === id) return done(null, clients[i]);
+  }
+  return done(new Error('Client Not Found'));
+};
+
+module.exports.findByClientId = (clientId, done) => {
+  for (let i = 0, len = clients.length; i < len; i++) {
+    if (clients[i].clientId === clientId) return done(null, clients[i]);
+  }
+  return done(new Error('Client Not Found'));
+};
